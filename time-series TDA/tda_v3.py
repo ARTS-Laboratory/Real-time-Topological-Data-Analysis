b2913a767a16aa32731249ba6ac2f71764e24b57
Nile <dncoble@gmail.com>
2023-06-22 15:26:30 -0400

    v3

--- time-series TDA/tda_v3.py
+++ time-series TDA/tda_v3.py
@@ -9,7 +9,7 @@ import persim as persim
 from scipy import signal
 from matplotlib.gridspec import GridSpec
 """
-TDA on DROPBEAR, varying parameters and making an animation
+Plotting maximum persistence against displacement
 """
 #%%
 # set default fonts and plot colors
@@ -74,24 +74,24 @@ X = X[:n_samples]
 y = y[:n_samples]
 t = tau*np.arange(n_samples)
 #%% TDA
-save_results = []
-for i in range(n_windows):
-    #%%
-    sig = X[i*s_n:i*s_n+2*w_n]
-    data = np.lib.stride_tricks.sliding_window_view(sig, (w_n), writeable=False)
-    results = ripser.ripser(data, maxdim=2)['dgms']
-    save_results.append(results)
-    print(i)
-#%% save results
-sv = []
-for results in save_results:
-    h0 = results[0]
-    h1 = results[1]
-    h2 = results[2]
+# save_results = []
+# for i in range(n_windows):
+#     #%%
+#     sig = X[i*s_n:i*s_n+2*w_n]
+#     data = np.lib.stride_tricks.sliding_window_view(sig, (w_n), writeable=False)
+#     results = ripser.ripser(data, maxdim=2)['dgms']
+#     save_results.append(results)
+#     print(i)
+# #%% save results
+# sv = []
+# for results in save_results:
+#     h0 = results[0]
+#     h1 = results[1]
+#     h2 = results[2]
     
-    sv.append([h0.tolist(), h1.tolist(), h2.tolist()])
-with open('./processed TDA/dropbear.json', 'w') as f:
-    json.dump(sv, f)
+#     sv.append([h0.tolist(), h1.tolist(), h2.tolist()])
+# with open('./processed TDA/dropbear.json', 'w') as f:
+#     json.dump(sv, f)
 #%% load results
 with open('./processed TDA/dropbear.json', 'r') as f:
     results = json.load(f)
@@ -138,80 +138,36 @@ def animate(i):
     l3.set_data(t_sig, y_sig)
     ax2.set_xlim((t_sig[0], t_sig[-1]))
     ax3.set_xlim((t_sig[0], t_sig[-1]))
-#%% animation frames 
-for i in range(0, len(save_results), 2):
-    fig = plt.figure(figsize=(5, 6), constrained_layout=True)
-    gs = GridSpec(3, 2, figure=fig)
-    ax1 = fig.add_subplot(gs[0:2,:])
-    ax2 = fig.add_subplot(gs[2,0])
-    ax3 = fig.add_subplot(gs[2,1])
+#%% maximum H1 persistence 
+h1persistence = np.zeros((len(save_results)))
+h1birth = np.zeros((len(save_results)))
+h1death = np.zeros((len(save_results)))
+h1_t = np.zeros((len(save_results)))
+for i, r in enumerate(save_results):
+    h1 = r[1]
+    h1_persistence = h1[:,1] - h1[:,0]
+    h1argmax = np.argmax(h1_persistence)
+    h1birth[i] = h1[h1argmax, 0]
+    h1death[i] = h1[h1argmax, 1]
+    h1persistence[i] = h1_persistence[h1argmax]
+    h1_t[i] = t[i*s_n+2*w_n]
+#%%
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(5, 6), sharex=True)
 
-    ax2.set_xlabel('time (s)')
-    ax2.set_ylabel('acceleration (m/s^2)')
-    ax3.set_xlabel('time')
-    ax3.set_ylabel('displacement (m)')
-    
-    ax2.set_ylim((np.min(X), np.max(X)))
-    ax3.set_ylim((0, .25))
-    
-    ax2.set_xticks([])
-    ax3.set_xticks([])
-    
-    l2, = ax2.plot([],[], c=cc[0])
-    l3, = ax3.plot([],[], c=cc[1])
-    
-    results = save_results[i]
-    sig = X[i*s_n:i*s_n+2*w_n]
-    y_sig = y[i*s_n:i*s_n+2*w_n]
-    t_sig = t[i*s_n:i*s_n+2*w_n]
-    
-    persim.plot_diagrams(results,show=False, xy_range=[0, 220, 0, 220], lifetime=False,ax=ax1)
-    l2.set_data(t_sig, sig)
-    l3.set_data(t_sig, y_sig)
-    ax2.set_xlim((t_sig[0], t_sig[-1]))
-    ax3.set_xlim((t_sig[0], t_sig[-1]))
-    
-    fig.savefig('./figs/animation frames/%04d.png'%(i//2), dpi=300)
-    plt.close('all')
-    
-    print(i)
-#%% all frames
-for i in range(0, len(save_results)):
-    fig = plt.figure(figsize=(5, 6), constrained_layout=True)
-    gs = GridSpec(3, 2, figure=fig)
-    ax1 = fig.add_subplot(gs[0:2,:])
-    ax2 = fig.add_subplot(gs[2,0])
-    ax3 = fig.add_subplot(gs[2,1])
+ax1.set_ylabel('pin location (m)')
+ax2.set_ylabel('Max. H1 persistence')
+ax3.set_ylabel('H1 birth')
+ax4.set_ylabel('H1 death')
+ax4.set_xlabel('time (s)')
+ax4.set_xlim((t[0], t[-1]))
 
-    ax2.set_xlabel('time (s)')
-    ax2.set_ylabel('acceleration (m/s^2)')
-    ax3.set_xlabel('time')
-    ax3.set_ylabel('displacement (m)')
-    
-    ax2.set_ylim((np.min(X), np.max(X)))
-    ax3.set_ylim((0, .25))
-    
-    ax2.set_xticks([])
-    ax3.set_xticks([])
-    
-    l2, = ax2.plot([],[], c=cc[0])
-    l3, = ax3.plot([],[], c=cc[1])
-    
-    results = save_results[i]
-    sig = X[i*s_n:i*s_n+2*w_n]
-    y_sig = y[i*s_n:i*s_n+2*w_n]
-    t_sig = t[i*s_n:i*s_n+2*w_n]
-    
-    persim.plot_diagrams(results,show=False, lifetime=False,ax=ax1)
-    l2.set_data(t_sig, sig)
-    l3.set_data(t_sig, y_sig)
-    ax2.set_xlim((t_sig[0], t_sig[-1]))
-    ax3.set_xlim((t_sig[0], t_sig[-1]))
-    
-    fig.savefig('./figs/all frames/%04d.png'%(i), dpi=300)
-    plt.close('all')
-    
-    print(i)
+ax1.plot(t, y, label='pin location')
+ax2.plot(h1_t, h1persistence)
+ax3.plot(h1_t, h1birth)
+ax4.plot(h1_t, h1death)
+
+plt.tight_layout()
+plt.savefig('./figs/persistence.png', dpi=500)
 #%%
 # import os
 # for file in os.listdir('./figs'):
